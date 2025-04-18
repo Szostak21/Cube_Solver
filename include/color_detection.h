a5d50a45a37286bef299ceb274f2c3eef9d135bb
Szostak21 <pawelszostak21@gmail.com>
2025-04-18 23:15:34 +0200

    color detection (not tested)

--- include/color_detection.h
+++ include/color_detection.h
@@ -8,7 +8,24 @@ class ColorDetector {
 public:
     ColorDetector();
     ~ColorDetector();
+
+    std::vector<cv::Rect> detectColors(const cv::Mat &frame);
+
+    cv::Mat preprocessImage(const cv::Mat &frame);
+
 private:
+    cv::Scalar lower_red = cv::Scalar(0, 120, 70);
+    cv::Scalar upper_red = cv::Scalar(10, 255, 255);
+    cv::Scalar lower_green = cv::Scalar(40, 70, 70);
+    cv::Scalar upper_green = cv::Scalar(90, 255, 255);
+    cv::Scalar lower_blue = cv::Scalar(100, 150, 0);
+    cv::Scalar upper_blue = cv::Scalar(140, 255, 255);
+    cv::Scalar lower_yellow = cv::Scalar(20, 100, 100);
+    cv::Scalar upper_yellow = cv::Scalar(30, 255, 255);
+    cv::Scalar lower_orange = cv::Scalar(10, 100, 20);
+    cv::Scalar upper_orange = cv::Scalar(25, 255, 255);
+    cv::Scalar lower_white = cv::Scalar(0, 0, 200);
+    cv::Scalar upper_white = cv::Scalar(180, 20, 255);
 
 };
 
--- src/vision/color_detection.cpp
+++ src/vision/color_detection.cpp
@@ -1,13 +1,49 @@
 #include <color_detection.h>
-#include <opencv2/opencv.hpp>
-#include <vector>
-
 
 ColorDetector::ColorDetector() {
-
 }
 
 ColorDetector::~ColorDetector() {
 
 }
 
+cv::Mat ColorDetector::preprocessImage(const cv::Mat &frame) {
+    cv::Mat hsv;
+    cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);   //convert BGR to HSV to use in different lightning 
+    return hsv;
+}
+
+std::vector<cv::Rect> ColorDetector::detectColors(const cv::Mat &frame){
+
+    cv::Mat hsv = preprocessImage(frame);
+
+    std::vector<cv::Rect> detectedRegions;
+
+    std::vector<std::pair<std::string, std::pair<cv::Scalar, cv::Scalar>>> colorRanges = {  //vector of pairs of colors and its ranges
+        {"yellow", {lower_yellow, upper_yellow}},
+        {"green", {lower_green, upper_green}},
+        {"orange", {lower_orange, upper_orange}},
+        {"blue", {lower_blue, upper_blue}},
+        {"red", {lower_red, upper_red}},
+        {"white", {lower_white, upper_white}}
+    };
+
+    for (const auto& color : colorRanges){
+        cv::Mat mask;
+
+        cv::inRange(hsv, color.second.first, color.second.second, mask); //check where the color is in the image
+
+        cv::erode(mask, mask, cv::Mat(), cv::Point(-1, -1), 2); //removing noise and making color more visable
+        cv::dilate(mask, mask, cv::Mat(), cv::Point(-1, -1), 2); 
+
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE); //find the contours of the color
+        for (const auto& contour : contours){
+            if (cv::contourArea(contour) > 500){
+                cv::Rect boundingBox = cv::boundingRect(contour);
+                detectedRegions.push_back(boundingBox);
+            }
+        }
+    }
+    return detectedRegions;
+}
