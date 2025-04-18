d6fe12f8517036bb034d755fa74af69fe698a7ff
Szostak21 <pawelszostak21@gmail.com>
2025-04-18 19:43:06 +0200

    color detection start

---
+++ include/color_detection.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_DETECTION_H
+#define COLOR_DETECTION_H
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+class ColorDetector {
+public:
+    ColorDetector();
+    ~ColorDetector();
+private:
+
+};
+
+
+
+#endif
---
+++ src/vision/color_detection.cpp
@@ -0,0 +1,13 @@
+#include <color_detection.h>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+
+ColorDetector::ColorDetector() {
+
+}
+
+ColorDetector::~ColorDetector() {
+
+}
+
